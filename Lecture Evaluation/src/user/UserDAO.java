49b9d36220f6a32a24339b1819ea7c629c3cb42b
Gundue <66405643+Gundue@users.noreply.github.com>
2022-01-07 10:37:25 +0900

    Add files via upload

---
+++ Lecture Evaluation/src/user/UserDAO.java
@@ -0,0 +1,133 @@
+package user;
+
+import java.sql.*;
+
+import util.DatabaseUtil;
+
+public class UserDAO {
+	
+	// 아이디와 비밀번호를 받아 로그인
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					// 성공
+					return 1;
+				} else {
+					// 실패
+					return 0;
+				}
+			}
+			return -1; //아이디없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(rs != null) rs.close(); } catch (Exception e) {e.printStackTrace();}
+		}
+		return -2; //DB오류
+	}
+	
+	// 사용자의 정보를 받아 회원가입
+	public int join(UserDTO user) {
+		String SQL = "INSERT INTO USER VALUES (?,?,?,?, false)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserEmail());
+			pstmt.setString(4, user.getUserEmailHash());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(rs != null) rs.close(); } catch (Exception e) {e.printStackTrace();}
+		}
+		return -1; //회원가입 실패
+	}
+	
+	// 아이디값을 받아와 ㅣuser이메일 가져오는함수
+	 public String getUserEmail(String userID) {
+		 String SQL = "SELECT userEmail FROM USER WHERE userID = ?";
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				conn = DatabaseUtil.getConnection();
+				pstmt = conn.prepareStatement(SQL);
+				pstmt.setString(1, userID);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					return rs.getString(1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try { if(conn != null) conn.close(); } catch (Exception e) {e.printStackTrace();}
+				try { if(pstmt != null) pstmt.close(); } catch (Exception e) {e.printStackTrace();}
+				try { if(rs != null) rs.close(); } catch (Exception e) {e.printStackTrace();}
+			}
+			return null; //데이터베이스 오류
+	 }
+	
+	// 사용자의 이메일 인증확인
+	public boolean getUserEmailChecked(String userID) {
+		String SQL = "SELECT userEmailChecked FROM USER WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getBoolean(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(rs != null) rs.close(); } catch (Exception e) {e.printStackTrace();}
+		}
+		return false; //데이터베이스 오류
+	}
+	
+	// 사용자의 이메일 인증을 수행
+	public boolean setUserEmailChecked(String userID) {
+		String SQL = "UPDATE USER SET userEmailChecked = true WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) {e.printStackTrace();}
+			try { if(rs != null) rs.close(); } catch (Exception e) {e.printStackTrace();}
+		}
+		return false; //데이터베이스 오류
+	}
+}
